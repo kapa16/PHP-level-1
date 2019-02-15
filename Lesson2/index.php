e9143d353d524999f395e3d7e17dd4d49e39f7b7
kapa16 <kapa16@inbox.ru>
2019-02-15 19:37:28 +0500

    task4

--- Lesson2/index.php
+++ Lesson2/index.php
@@ -3,6 +3,7 @@
 require_once 'task1.php';
 require_once 'task2.php';
 require_once 'task3.php';
+require_once 'task4.php';
 
 
 
--- Lesson2/task4.php
+++ Lesson2/task4.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kapa
- * Date: 15.02.2019
- * Time: 19:25
- */
+//4. Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation),
+// где $arg1, $arg2 – значения аргументов, $operation – строка с названием операции.
+// В зависимости от переданного значения операции выполнить одну из арифметических операций
+// (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).
+
+function mathOperation($arg1, $arg2, $operation) {
+    switch ($operation) {
+        case '+':
+            return addition($arg1, $arg2);
+        case '-':
+            return subtraction($arg1, $arg2);
+        case '*':
+            return multiplication($arg1, $arg2);
+        case '/':
+            return division($arg1, $arg2);
+        default:
+            return 'unknown operation';
+    }
+}
+
+echo 'Задание 4.<br>';
+echo 'a = ' . $a . '<br>';
+echo 'b = ' . $b . '<br>';
+echo '+:  &nbsp;&nbsp;&nbsp;' . mathOperation($a, $b, '+') . '<br>';
+echo '-:  &nbsp;&nbsp;&nbsp;' . mathOperation($a, $b, '-') . '<br>';
+echo '*:  &nbsp;&nbsp;&nbsp;' . mathOperation($a, $b, '*') . '<br>';
+echo '/:  &nbsp;&nbsp;&nbsp;' . mathOperation($a, $b, '/') . '<hr>';
