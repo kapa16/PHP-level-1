d619e4dd1599021e0e04d9ef3478799045e4a45e
Pavel <kapa16@inbox.ru>
2019-02-15 15:49:03 +0500

    task3

--- Lesson2/task3.php
+++ Lesson2/task3.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 15.02.2019
- * Time: 15:36
- */
+//3. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.
+// Обязательно использовать оператор return.
+
+function addition(float $a, float $b)
+{
+    return $a + $b;
+}
+
+function subtraction(float $a, float $b)
+{
+    return $a - $b;
+}
+
+function multiplication(float $a, float $b)
+{
+    return $a * $b;
+}
+
+function division(float $a, float $b)
+{
+    return $a / $b;
+}
+
+$a = rand(-10, 10);
+$b = rand(-10, 10);
+echo 'Задание 3.<br>';
+echo 'a = ' . $a . '<br>';
+echo 'b = ' . $b . '<br>';
+echo compare($a, $b) . '<hr>';
