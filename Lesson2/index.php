5ec6764ca6327ab0c8b38489253e0655e37dbe1e
kapa16 <kapa16@inbox.ru>
2019-02-15 19:45:16 +0500

    task5

--- Lesson2/index.php
+++ Lesson2/index.php
@@ -4,6 +4,23 @@ require_once 'task1.php';
 require_once 'task2.php';
 require_once 'task3.php';
 require_once 'task4.php';
+require_once 'task5.php';
 
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<footer>Copyright &copy;<?=$year?></footer>
+<hr>
+</body>
+</html>
 
 
