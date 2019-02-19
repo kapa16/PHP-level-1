165b894552a8466de6a9b591769b7c68ee2658db
Pavel <kapa16@inbox.ru>
2019-02-19 11:30:18 +0500

    task6

--- Lesson3/index.php
+++ Lesson3/index.php
@@ -5,3 +5,4 @@ require_once __DIR__ . '/task2.php';
 require_once __DIR__ . '/task3.php';
 require_once __DIR__ . '/task4.php';
 require_once __DIR__ . '/task5.php';
+require_once __DIR__ . '/task6.php';
--- Lesson3/task6.php
+++ Lesson3/task6.php
@@ -1,7 +1,71 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 19.02.2019
- * Time: 11:00
- */
+
+//6. В имеющемся шаблоне сайта заменить статичное меню (ul – li) на генерируемое через PHP.
+// Необходимо представить пункты меню как элементы массива и вывести их циклом.
+// Подумать, как можно реализовать меню с вложенными подменю? Попробовать его реализовать.
+
+$menuContent = [
+    [
+        'title' => 'Главаная',
+        'link'  => '/',
+    ],
+    [
+        'title'    => 'Каталог',
+        'link'     => '/catalog/',
+        'children' => [
+            [
+                'title'    => 'Женская одежда',
+                'link'     => '/catalog/woman/',
+                'children' => [
+                    [
+                        'title' => 'Куртки',
+                        'link'  => '/catalog/woman/jacket/',
+                    ],
+                    [
+                        'title' => 'Блузки',
+                        'link'  => '/catalog/woman/blouse/',
+                    ],
+                    [
+                        'title' => 'Брюки',
+                        'link'  => '/catalog/woman/pants/',
+                    ],
+                ],
+            ],
+            [
+                'title' => 'Мужская одежда',
+                'link'  => '/catalog/man/',
+            ],
+            [
+                'title' => 'Детская одежда',
+                'link'  => '/catalog/child/',
+            ],
+        ],
+    ],
+    [
+        'title' => 'Контакты',
+        'link'  => '/contacts/',
+    ],
+    [
+        'title' => 'О нас',
+        'link'  => '/about/',
+    ],
+];
+
+function createMenu(array $menuContent)
+{
+    $html = '<ul>';
+    foreach ($menuContent as $key => $value) {
+        $subMenu = '';
+        if (isset($value['children']) && is_array($value['children'])) {
+            $subMenu = createMenu($value['children']);
+        }
+        $title = htmlspecialchars($value['title']);
+        $link = htmlspecialchars($value['link']);
+        $html .= '<li><a href="' . $link . '">' . $title . '</a>' . $subMenu . '</li>';
+
+    }
+    $html .= '</ul>';
+    return $html;
+}
+
+echo createMenu($menuContent);
