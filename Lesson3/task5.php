5966a82b32d19fd74f7c264f930ea984bbacd5fd
Pavel <kapa16@inbox.ru>
2019-02-19 10:59:59 +0500

    task5

--- Lesson3/task5.php
+++ Lesson3/task5.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 19.02.2019
- * Time: 10:52
- */
+
+//5. Написать функцию, которая заменяет в строке пробелы на подчеркивания и возвращает видоизмененную строчку.
+
+echo 'Задание 5. <br>';
+
+function changeSpaces($str) {
+    $arrStr = explode(' ', $str);
+    return implode('_', $arrStr);
+}
+
+echo changeSpaces('Написать функцию, которая заменяет в строке пробелы на подчеркивания.') . '<br>';
