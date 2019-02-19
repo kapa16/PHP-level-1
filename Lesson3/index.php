1231490115d4cff1e240f1f1f26fb431aec25d8d
Pavel <kapa16@inbox.ru>
2019-02-19 11:40:06 +0500

    task 7

--- Lesson3/index.php
+++ Lesson3/index.php
@@ -6,3 +6,5 @@ require_once __DIR__ . '/task3.php';
 require_once __DIR__ . '/task4.php';
 require_once __DIR__ . '/task5.php';
 require_once __DIR__ . '/task6.php';
+require_once __DIR__ . '/task7.php';
+require_once __DIR__ . '/task8.php';
