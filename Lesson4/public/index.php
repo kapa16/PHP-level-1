dbbfe731117525d8267832a6b9b837d8ded4a293
Pavel <kapa16@inbox.ru>
2019-02-22 15:36:13 +0500

    create gallegy html

--- Lesson4/public/index.php
+++ Lesson4/public/index.php
@@ -2,12 +2,24 @@
 
 require_once (__DIR__ . '/../config/config.php');
 
-$templateData = [
-  'title' => 'Gallery',
-  'header' => 'Галерея',
-  'currentYear' => date('Y')
-];
+$imagesSrc = getPhotos(WWW_DIR . 'img');
+$galleryHtml = '';
+foreach ($imagesSrc as $imgSrc) {
+    $imageData = [
+      'imageSource' => $imgSrc,
+      'imageAlt' => 'photo',
+      'imageClass' => 'gallery__image'
+    ];
+    $galleryHtml .= render(TEMPLATE_DIR . 'imageCard.tpl', $imageData);
+}
+$galleryHtml = render(TEMPLATE_DIR . 'gallery.tpl', ['contentGallery' => $galleryHtml]);
 
-echo getPhotos(WWW_DIR . 'img/');
+
+$templateData = [
+    'title' => 'Gallery',
+    'header' => 'Галерея',
+    'currentYear' => date('Y'),
+    'content' => $galleryHtml
+];
 
 echo render(TEMPLATE_DIR . 'index.tpl', $templateData);
