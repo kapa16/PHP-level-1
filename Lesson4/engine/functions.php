4523f328f86a760b03a88e782d865c19ca124941
Pavel <kapa16@inbox.ru>
2019-02-22 13:59:30 +0500

    write render, add getPhotos

--- Lesson4/engine/functions.php
+++ Lesson4/engine/functions.php
@@ -1,15 +1,47 @@
 <?php
 
 function render($file, $variables = []) {
-    if (empty($file)) {
-        return 'Пустое имя файла';
+    if (!is_file($file)) {
+        return 'File ' . $file . ' not found';
     }
 
+    if (filesize($file) === 0) {
+        return 'File ' . $file . ' is empty';
+    }
+    
     $template = file_get_contents($file);
 
-    if (empty($template)) {
-        return 'Пустой файл ' . $file;
+
+    if (empty($variables)) {
+        return $template;
     }
 
-    if ()
+    foreach ($variables as $key => $value) {
+        if (empty($value)) {
+            continue;
+        }
+        $searchKey = '{{' . strtoupper($key) . '}}';
+        $template = str_replace($searchKey, $value, $template);
+    }
+
+    return $template;
+}
+
+function getPhotos($dirName) {
+    if (!is_dir($dirName)) {
+        return 'Directory ' . $dirName . ' not found';
+    }
+
+    $files = scandir($dirName);
+
+    foreach ($files as $fileName) {
+        $fullPath = $dirName . '/' . $fileName;
+        if (!is_file($fullPath)) {
+            continue;
+        }
+        if (!exif_imagetype($fullPath)) {
+            continue;
+        }
+        var_dump($fullPath);
+    }
 }
