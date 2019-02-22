d04147ca9f8dd9344548d0d4b1045f5b54e9e310
Pavel <kapa16@inbox.ru>
2019-02-22 14:01:58 +0500

    require engine functions

--- Lesson4/config/config.php
+++ Lesson4/config/config.php
@@ -6,3 +6,5 @@ define('CONFIG_DIR', SITE_DIR . 'config/');
 define('DATA_DIR', SITE_DIR . 'dta/');
 define('WWW_DIR', SITE_DIR . 'public/');
 define('ENGINE_DIR', SITE_DIR . 'engine/');
+
+require_once(ENGINE_DIR . 'functions.php');
