6de15269a10fe977d5f5803fa1a7dacb79c50ddb
Pavel <kapa16@inbox.ru>
2019-02-22 12:22:30 +0500

    constants for path

--- Lesson4/config/config.php
+++ Lesson4/config/config.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 22.02.2019
- * Time: 12:10
- */
+
+define('SITE_DIR', __DIR__ . '/../');
+define('TEMPLATE_DIR', SITE_DIR . 'templates/');
+define('CONFIG_DIR', SITE_DIR . 'config/');
+define('DATA_DIR', SITE_DIR . 'dta/');
+define('WWW_DIR', SITE_DIR . 'public/');
+define('ENGINE_DIR', SITE_DIR . 'engine/');
