2cd3869468cf5d3eb05fb924ec8392897ced0368
Pavel <kapa16@inbox.ru>
2019-02-22 14:02:55 +0500

    render main template

--- Lesson4/public/index.php
+++ Lesson4/public/index.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 22.02.2019
- * Time: 12:10
- */
+
+require_once (__DIR__ . '/../config/config.php');
+
+$templateData = [
+  'title' => 'Gallery',
+  'header' => 'Галерея',
+  'currentYear' => date('Y')
+];
+
+echo getPhotos(WWW_DIR . 'img/');
+
+echo render(TEMPLATE_DIR . 'index.tpl', $templateData);
