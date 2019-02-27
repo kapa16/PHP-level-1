5f17669a89e1d2804a4512a8f1d8a15e2f9ffab8
Pavel <kapa16@inbox.ru>
2019-02-27 15:43:51 +0500

    page view full size image

--- Lesson5/public/image_view.php
+++ Lesson5/public/image_view.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 27.02.2019
- * Time: 13:45
- */
+
+require_once (__DIR__ . '/../config/config.php');
+
+$photoId = +$_GET['photo-id'];
+$image = getPhotos($photoId);
+$galleryHtml = getHtmlGallery($image, 'image_view.tpl');
+
+
+$templateData = [
+    'title' => 'Gallery',
+    'header' => 'Просмотр полноразмерного изображения',
+    'currentYear' => date('Y'),
+    'content' => $galleryHtml
+];
+
+echo render(TEMPLATE_DIR . 'index.tpl', $templateData);
+
+function setPhotoViews() {
+    
+}
