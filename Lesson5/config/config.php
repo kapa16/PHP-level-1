ef3e59ac9714ce1961bf16a22a8c36b34eb1362d
Pavel <kapa16@inbox.ru>
2019-02-27 15:39:21 +0500

    add config for DB & method sort image

--- Lesson5/config/config.php
+++ Lesson5/config/config.php
@@ -7,4 +7,14 @@ define('DATA_DIR', SITE_DIR . 'dta/');
 define('WWW_DIR', SITE_DIR . 'public/');
 define('ENGINE_DIR', SITE_DIR . 'engine/');
 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'geek_brains');
+define('DB_PASS', '123123');
+define('DB_NAME', 'geek_brains_shop');
+
+define('SORT_IMAGE_BY_VIEWS', 'views');
+define('SORT_IMAGE_BY_NAME', 'title');
+define('SORT_IMAGE_BY_ID', 'id');
+
+require_once(ENGINE_DIR . 'db.php');
 require_once(ENGINE_DIR . 'functions.php');
