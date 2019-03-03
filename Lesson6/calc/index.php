b682c7fb9724d3ef3becb5f01f02fd9d8babbadb
kapa16 <kapa16@inbox.ru>
2019-03-03 08:30:21 +0500

    task1

--- Lesson6/calc/index.php
+++ Lesson6/calc/index.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kapa
- * Date: 02.03.2019
- * Time: 20:46
- */
+header('Location: /index.php');
+if (empty($_POST) || empty($_POST['x1']) || empty($_POST['y1'])) {
+    exit;
+}
+
+$x = $_POST['x1'];
+$y = $_POST['y1'];
+
+switch ($_POST['sign']){
+    case 'addition':
+        echo $x + $y;
+        break;
+    case 'subtraction':
+        echo $x + $y;
+        break;
+    case 'multiplication':
+        echo $x + $y;
+        break;
+    case 'plus':
+        echo $x + $y;
+        break;
+    default:
+        echo 'Неизвестная операция';
+}
+
--- Lesson6/index.php
+++ Lesson6/index.php
@@ -1,7 +1,52 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kapa
- * Date: 02.03.2019
- * Time: 20:42
- */
+if (empty($_POST) || empty($_POST['x1']) || empty($_POST['y1'])) {
+    exit;
+}
+
+$x = $_POST['x1'];
+$y = $_POST['y1'];
+
+switch ($_POST['sign']){
+    case 'addition':
+        echo $x + $y;
+        break;
+    case 'subtraction':
+        echo $x + $y;
+        break;
+    case 'multiplication':
+        echo $x + $y;
+        break;
+    case 'plus':
+        echo $x + $y;
+        break;
+    default:
+        echo 'Неизвестная операция';
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="/" method="post" name="calc">
+    <input type="number" name="x1">
+    <select name="sign">
+        <option value="addition">+</option>
+        <option value="subtraction">-</option>
+        <option value="multiplication">*</option>
+        <option value="plus">/</option>
+    </select>
+    <input type="number" name="y1">
+    <button type="submit">Расчет</button>
+</form>
+
+
+</body>
+</html>
