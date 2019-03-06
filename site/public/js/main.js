438ee350214b941aa46758ebeb51f3b8ffd7f9d7
Pavel <kapa16@inbox.ru>
2019-03-06 17:35:20 +0500

    add javascript

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -3,3 +3,12 @@
 // const block = document.querySelector('#gallery');
 //
 // const gallery = new Gallery(block);
+
+window.onload = () => {
+  document.getElementById('valid').addEventListener('submit', e => {
+    // let valid = new Validator('valid');
+    // if (!valid.valid){
+    //   e.preventDefault();
+    // }
+  })
+};
