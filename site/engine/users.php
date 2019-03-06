348a62691e08ea8f5f2f253daece156fa7c23593
Pavel <kapa16@inbox.ru>
2019-03-06 17:39:40 +0500

    work with users

--- site/engine/users.php
+++ site/engine/users.php
@@ -1,2 +1,67 @@
 <?php
 
+function getDataFromPost($postParams, $fields)
+{
+    $data = [];
+    foreach ($fields as $field) {
+        $data[$field] = empty($postParams[$field]) ? '' : $postParams[$field];
+    }
+    return $data;
+}
+
+function registrationUser($postParams)
+{
+    $result = ['error' => false, 'data' => ''];
+
+    $fields = ['login', 'email', 'name', 'lastname', 'password'];
+    $userData = getDataFromPost($postParams, $fields);
+    $userData['password'] = password_hash($userData['password'], PASSWORD_DEFAULT);
+
+    $user = findUser($userData['login']);
+    if (!empty($user)) {
+        $result['error'] = true;
+        $result['data'] = 'Пользователь с логином ' . $userData['login'] . ' уже существует';
+        return $result;
+    }
+
+    $sql = 'INSERT INTO `geek_brains_shop`.`users` (`login`, `email`, `name`, `lastname`, `password`) VALUES (?, ?, ?, ?, ?)';
+    $resultSql = executePrepareQuery($sql, array_merge(['sssss'], $userData), true);
+    if ($resultSql === 1) {
+        $result['data'] = $userData;
+    } else {
+        $result['error'] = true;
+        $result['data'] = 'Пользователь не зарегистрирован';
+    }
+    return $result;
+}
+
+function findUser($userLogin)
+{
+    $sql = "SELECT * FROM `geek_brains_shop`.`users` WHERE `login`='" . $userLogin . "';";
+    return getAssocData($sql);
+}
+
+function createSession($userData)
+{
+    foreach ($userData as $paramName => $data) {
+        $_SESSION[$paramName] = $data;
+    }
+}
+
+function loginUser($postParams)
+{
+    $fields = ['login', 'password'];
+    $userData = getDataFromPost($postParams, $fields);
+
+    $user = findUser($userData['login'])[0];
+
+    if (!$user) {
+        return false;
+    }
+
+    if (password_verify($userData['password'], $user['password'])) {
+        return $user;
+    }
+
+    return false;
+}
