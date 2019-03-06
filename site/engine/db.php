a6945944d9044ee3ecc62bee635e9e36911a2c59
Pavel <kapa16@inbox.ru>
2019-03-06 17:38:53 +0500

    add prepare SQL query

--- site/engine/db.php
+++ site/engine/db.php
@@ -3,10 +3,14 @@
 function createConnection()
 {
     $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    if (!$link) {
-        echo "Ошибка, невозможно установить соединение <br>";
-        exit;
+    if (!$link)
+    {
+        $error = mysqli_connect_error();
+        $errno = mysqli_connect_errno();
+        print "$errno: $error\n";
+        exit();
     }
+
     mysqli_query($link, "SET CHARACTER SET 'utf8'");
     return $link;
 }
@@ -33,3 +37,55 @@ function getAssocData($query)
 
     return $data_array;
 }
+
+function executePrepareQuery($sql, $params, $close){
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    $stmt = $mysqli->prepare($sql) or die ("Failed to prepared the statement!");
+
+    call_user_func_array(array($stmt, 'bind_param'), refValues($params));
+
+    $stmt->execute();
+
+    if($close){
+        $result = $mysqli->affected_rows;
+//        var_dump($stmt->insert_id);
+    } else {
+        $meta = $stmt->result_metadata();
+
+        $parameters = [];
+        while ( $field = $meta->fetch_field() ) {
+            $parameters[] = &$row[$field->name];
+        }
+
+        call_user_func_array(array($stmt, 'bind_result'), refValues($parameters));
+
+        $results =[];
+        while ( $stmt->fetch() ) {
+            $x = array();
+            foreach( $row as $key => $val ) {
+                $x[$key] = $val;
+            }
+            $results[] = $x;
+        }
+
+        $result = $results;
+    }
+
+    $stmt->close();
+    $mysqli->close();
+
+    return  $result;
+}
+
+function refValues($arr){
+    if (strnatcmp(phpversion(),'5.3') >= 0) //Reference is required for PHP 5.3+
+    {
+        $refs = array();
+        foreach($arr as $key => $value)
+            $refs[$key] = &$arr[$key];
+        return $refs;
+    }
+    return $arr;
+}
+?>
