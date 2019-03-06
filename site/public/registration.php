b9b92e2dc5c7978238ba8bb1880ee5a922f9641d
Pavel <kapa16@inbox.ru>
2019-03-06 17:37:50 +0500

    add registration

--- site/public/registration.php
+++ site/public/registration.php
@@ -1,2 +1,21 @@
 <?php
 
+require_once __DIR__ . '/../config/config.php';
+
+if (!empty($_POST)) {
+    $result = registrationUser($_POST);
+    if ($result['error']) {
+        echo $result['data'];
+    } else {
+        createSession($result['data']);
+        header('Location: /');
+    }
+}
+
+$registrationHtml = render(REGISTRATION_TEMPLATE);
+
+$templateData['title'] = 'Registration';
+$templateData['header'] = 'Регистрация';
+$templateData['content'] = $registrationHtml;
+
+echo render(INDEX_TEMPLATE, $templateData);
